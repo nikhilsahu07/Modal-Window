26aba5accfc451172a0de23378c511583d47ccc5
Nikhil Sahu <heynikhil07@gmail.com>
2023-04-18 23:31:12 +0530

    Completed the project #2 and learned keyboard eventhandler and querySelectorAll and many more concepts

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+//querySelectorAll returns a array of list of all element of class named
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
+}
+
+for (let i = 0; i < btnShowModal.length; i++) {
+    btnShowModal[i].addEventListener('click', openModal);
+
+    btnCloseModal.addEventListener('click', closeModal);
+
+    overlay.addEventListener('click', closeModal);
+}
+
+//keypress event handler
+
+document.addEventListener('keydown', function (event) {
+    if (!modal.classList.contains('hidden') && event.key === 'Escape') {
+        closeModal();
+    }
+})
